5c3d96e04b8df295fd8b1b314aa88d2e945be34e
Caio Henrique <caio.hnascimento98@gmail.com>
2022-03-04 20:08:27 -0300

    Inicio do projeto de React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+const api = {
+  key: "b002e52ade95bcd77704fc0dc70380a8",
+  base: "http://api.openweathermap.org/data/2.5/",
+}
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box"> 
+            <input type="text" className="search-bar" placeholder="search..."/>
+        </div>
+      </main>
     </div>
   );
 }
